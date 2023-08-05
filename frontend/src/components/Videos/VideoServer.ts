29afc302bb6b954a4a6bea4e6d5538d393007865
martin-bass <martin_reym@hotmail.com>
2023-08-05 13:52:42 -0300

    prueba update env

--- frontend/src/components/Videos/VideoServer.ts
+++ frontend/src/components/Videos/VideoServer.ts
@@ -2,7 +2,8 @@ import axios from "axios";
 import { Video } from "./Video";
 
 //const api = "http://localhost:4000";
-const api = "https://mern-appdevideos-production-09ca.up.railway.app";
+//const api = "https://mern-appdevideos-production-09ca.up.railway.app";
+const api = "https://mernappvideos-api.onrender.com/";
 
 export const getVideos = async () => {
   return await axios.get<Video[]>(`${api}/videos`);
